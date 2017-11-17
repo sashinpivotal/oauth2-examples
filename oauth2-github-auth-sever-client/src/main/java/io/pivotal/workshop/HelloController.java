82e9f3da2f794759380a32d178a3932f707c0985
sashinpivotal <sashin@pivotal.io>
2017-11-17 05:53:03 -0500

    Added more TODO items

--- oauth2-github-auth-sever-client/src/main/java/io/pivotal/workshop/HelloController.java
+++ oauth2-github-auth-sever-client/src/main/java/io/pivotal/workshop/HelloController.java
@@ -15,10 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 // TODO-01: In the console of "oauth2-github-auth-server-client" application, 
 //          observe that the access token obtained from the GitHub authorization
 //          server gets displayed 
-// TODO-02: Using the token, access the resource server directly using curl or postman
-// curl -H "Authorization: Bearer <Token>" http://localhost:8001/resource-server/resource-in-server
+// TODO-02: Using that token, access the resource server directly using curl or postman
+//          curl -H "Authorization: Bearer <Token>" http://localhost:8001/resource-server/resource-in-server
+//          You should see "Message from resource server" message
 // TODO-03: Access the resource server from the "oauth2-github-auth-server-client" application
 //          http://localhost:8080/resource-in-client either via browser or curl/postman
+//          You should see "Message from resource server" message - this verifies that
+// TODO-04: Go to GitHub.com and create your own oauth2 application, which results in its
+//          own "clientId" and "clientSecret". Use these new values in the the "application.yml"
+//          file of the "oauth2-github-auth-server-client" application
 @RestController
 @EnableOAuth2Sso
 public class HelloController extends WebSecurityConfigurerAdapter {
